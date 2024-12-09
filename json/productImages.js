a7301b99e0f53c50c670dacdcde51af058965c95
Prachi Tavse <prachi.pt83@gmail.com>
2024-12-10 00:31:09 +0530

    Added data

--- json/productImages.js
+++ json/productImages.js
@@ -3,7 +3,13 @@ const productImages = {
     "PID001": { "category": "Bag", "imagePath": "../products/Bag.png" },  // Corrected path
     "PID002": { "category": "Shoes", "imagePath": "../products/Shoes.png" },
     "PID003": { "category": "Perfume", "imagePath": "../products/Perfume.png" },
-    "PID004": { "category": "Tshirt", "imagePath": "../products/Tshirt.png" }
+    "PID004": { "category": "Tshirt", "imagePath": "../products/Tshirt.png" },
+    "PID005": { "category": "Bottle", "imagePath": "../products/Bottle.png" },
+    "PID006": { "category": "Cosmetics", "imagePath": "../products/Cosmetics.png" },
+    "PID007": { "category": "Headphones", "imagePath": "../products/Headphones.png" },
+    "PID008": { "category": "Speaker", "imagePath": "../products/Speaker.png" },
+    "PID009": { "category": "Sunglasses", "imagePath": "../products/Sunglasses.png" },
+    "PID0010": { "category": "Watch", "imagePath": "../products/Watch.png" }
 };
 
 // Export the productImages object so it can be used in other files
